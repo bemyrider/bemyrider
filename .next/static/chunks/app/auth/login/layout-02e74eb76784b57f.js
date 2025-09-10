06657a6ec35646b93825c3676c52958e451b64ed
bemyrider <bemyrider@gmail.com>
2025-09-10 15:39:56 +0200

    fix: rimuovi file di build .next/ dal repository
    - Rimosso 365+ file di build dalla cartella .next/
    - File di cache webpack, immagini ottimizzate, manifest
    - Questi file vengono rigenerati automaticamente dal build
    - Risolve warning GitHub LFS per file troppo grandi
    - Migliora performance repository e velocità di clone
    
    BREAKING CHANGE: I file di build non sono più nel repository

--- .next/static/chunks/app/auth/login/layout-02e74eb76784b57f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[892,663,426,847,720,329,738,262,451,560],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
